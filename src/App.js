0910c388b945c7ae614a682b0c2cc7ac1b02c1ae
Amber-Pittman <amber@amberpittman.com>
2019-08-28 18:58:10 -0400

    Using setState to pull image title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import PhotoCard from "./components/PhotoCard";
 import "./App.css";
 
 function App() {
   console.log("top")
+  const [title, setTitle] = useState(""); // Use empty string to avoid errors
+  
   axios.get("https://api.nasa.gov/planetary/apod?api_key=y8Ih2vW2O2XdWTf3knaCA1DOoETsWABCqkpSc9Kq&date=2019-07-28")
     .then(
       response => {
         console.log(response.data);
+        setTitle(response.data.title); //Sets title to be that string
       }
     );
     console.log("bottom");
@@ -19,7 +22,7 @@ function App() {
         app! Have fun 🚀!
       </p>
 
-      <PhotoCard title={"Props Title"} url={"https://api.nasa.gov/planetary/apod?api_key=y8Ih2vW2O2XdWTf3knaCA1DOoETsWABCqkpSc9Kq&date=2019-07-28"} explanation={"some explanation"} />
+      <PhotoCard title={title} url={"https://api.nasa.gov/planetary/apod?api_key=y8Ih2vW2O2XdWTf3knaCA1DOoETsWABCqkpSc9Kq&date=2019-07-28"} explanation={"some explanation"} />
     </div>
   );
 }
