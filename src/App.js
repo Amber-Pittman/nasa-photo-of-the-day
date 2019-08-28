3a40e1ddab8b17747b3f635e682e031a3b606130
Amber-Pittman <amber@amberpittman.com>
2019-08-28 19:05:19 -0400

    Made code dry by changing const title to data & updating

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 
 function App() {
   console.log("top")
-  const [title, setTitle] = useState(""); // Use empty string to avoid errors
+  const [data, setData] = useState({}); // Use empty object brackets to keep data type consistent
   
   axios.get("https://api.nasa.gov/planetary/apod?api_key=y8Ih2vW2O2XdWTf3knaCA1DOoETsWABCqkpSc9Kq&date=2019-07-28")
     .then(
       response => {
         console.log(response.data);
-        setTitle(response.data.title); //Sets title to be that string
+        setData(response.data); // Data object give us access to title, url, explanation & put in <PhotoCard />
       }
     );
     console.log("bottom");
@@ -22,7 +22,7 @@ function App() {
         app! Have fun 🚀!
       </p>
 
-      <PhotoCard title={title} url={"https://api.nasa.gov/planetary/apod?api_key=y8Ih2vW2O2XdWTf3knaCA1DOoETsWABCqkpSc9Kq&date=2019-07-28"} explanation={"some explanation"} />
+      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} />
     </div>
   );
 }
