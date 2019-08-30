010a1b75907cc7e7e22b363ab2f9578364756448
Amber-Pittman <amber@amberpittman.com>
2019-08-29 21:33:46 -0400

    Fixing styles a bit. Commented out App.css

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import styled from "styled-components";
 
+
 const StyledContainer = styled.div`
   width: 100%;
   height: 100%;
+  margin: 0 auto;
+  background-color: ghostwhite;
 `;
 
 const StyledH2 = styled.h2`
-  margin: 1.6em;
+  margin: 0 0 1.2em 0;
+  padding-top: 1.2em;
   text-align: center;
   font-size: 4.8em;
-  background: -webkit-linear-gradient(#333, #eee);
+  background: -webkit-linear-gradient(#8533ff, #b380ff, #f0e6ff);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 `;
 
 const StyledImage = styled.img`
-  width: 75%;
-  margin: 0 auto;
+  width: 60%;
+  margin-left: 20%;
+  margin-right: 20%
   border-radius: 25px;
   display: flex;
   justify-content: center;
 `;
 
-const StyledPhotoDate = styled.p``;
+const StyledPhotoDate = styled.p`
+  margin: 0 auto;
+  font-weight: 700;
+  font-size: 2.4em;
+  display: flex;
+  justify-content: center;
+  color: #ac00e6;
+`;
 
-const StyledP = styled.p``;
+const StyledP = styled.p`
+  font-size: 1.2em;
+  margin-left: 20%;
+  margin-right: 20%
+  width: 60%;
+  line-height: 1.25;
+`;
 
 function PhotoCard(props) {
     console.log(props);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styled from "styled-components";
 import "./index.css";
 import App from "./App";
 
